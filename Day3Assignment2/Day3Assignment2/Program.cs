af2b2228a4119b3ec0f822735fbda9858c3ed41f
Your BhaktiIngole97 <ingolebhakti97@gmail.com>
2022-07-18 16:04:42 +0530

    UC-3-Compare two lengths

--- Day3Assignment2/Day3Assignment2/Program.cs
+++ Day3Assignment2/Day3Assignment2/Program.cs
@@ -1,5 +1,5 @@
-﻿int x1 = 2, x2 = 6, y1 = 3, y2 = 3;
-int x3 = 4, x4 = 7, y3 = 3, y4 = 5;
+﻿int x1 = 2, x2 = 5, y1 = 7, y2 = 6;
+int x3 = 3, x4 = 4, y3 = 3, y4 = 5;
 double line1 = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
 double line2 = Math.Sqrt(Math.Pow(x4 - x3, 2) + Math.Pow(y4 - y3, 2));
-Console.WriteLine(line1.Equals(line2));
+Console.WriteLine(line1.CompareTo(line2));
